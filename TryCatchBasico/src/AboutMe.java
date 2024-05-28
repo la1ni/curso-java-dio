12179e81db8700be3eab017c9369c0aeed475a3c
mariana lainara <marilainarasilva@hotmail.com>
2024-05-28 11:10:29 -0300

    Add: TryCatchBasico

---
+++ TryCatchBasico/src/AboutMe.java
@@ -0,0 +1,28 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.Locale;
+public class AboutMe {
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+            System.out.println("Digite seu primeiro nome: ");
+            String nome = scanner.nextLine();
+
+            System.out.println("Digite seu sobrenome: ");
+            String sobrenome = scanner.nextLine();
+
+            System.out.println("Digite sua idade: ");
+            int idade = scanner.nextInt();
+
+            System.out.println("Digite sua altura: ");
+            double altura = scanner.nextDouble();
+
+            scanner.close();
+            System.out.println("nome completo: "+ nome + " " + sobrenome);
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Os campos idade e altura precisam ser numéricos. Altura deve ser informada com ponto");
+        }
+
+    }
+}
