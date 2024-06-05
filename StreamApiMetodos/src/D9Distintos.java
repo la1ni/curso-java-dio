869102ff09457b96a51af880b1b3ffe2c999a529
la1ni <marilainarasilva@hotmail.com>
2024-06-05 16:16:49 -0300

    Update project StreamApiMetodos

--- StreamApiMetodos/src/D9Distintos.java
+++ StreamApiMetodos/src/D9Distintos.java
@@ -1,13 +1,24 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class D9Distintos {
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
-        List<Integer> listaOrdenada = (List<Integer>) numeros.stream().sorted();
+        List<Integer> listaOrdenada = numeros.stream().sorted().toList();
 
-        //List<Integer> repetidos = listaOrdenada.stream();
+        List<Integer> listaFinal = new ArrayList<>();
+
+        for (Integer i : listaOrdenada){
+            long contagem = listaOrdenada.stream().filter(n -> Objects.equals(n, i)).count();
+            if (contagem == 1) {
+                listaFinal.add(i);
+            }
+        }
+        System.out.println("Os números que não se repetem são: " + listaFinal);
+
 
     }
 }
