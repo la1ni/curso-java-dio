04c5ce7e749b8a13eca435a3b88057a9d317ab6b
la1ni <marilainarasilva@hotmail.com>
2024-06-12 09:07:49 -0300

    Add project DesafiosDeCodigoModuloPOO and README

---
+++ DesafiosDeCodigoModuloPOO/README.md
@@ -0,0 +1,9 @@
+# Desafios do Módulo de Programação Orientada a Objetos
+
+![Java](https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=openjdk&logoColor=white)
+
+## Desafio 1:
+`RegistroTransacoes`
+
+O programa gerencia o saldo de uma conta bancária com base nas transações informadas pelo usuário. Ele solicita um saldo inicial, o número de transações a serem realizadas e, para cada transação, o tipo (depósito ou saque) e o valor. Ao final, exibe o saldo final e a lista de transações.
+
---
+++ DesafiosDeCodigoModuloPOO/src/RegistroTransacoes.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class RegistroTransacoes {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double saldo = scanner.nextDouble();
+
+        int quantidadeTransacoes = scanner.nextInt();
+
+        List<String> transacoes = new ArrayList<>();
+
+        for (int i = 1; i <= quantidadeTransacoes; i++) {
+            char tipoTransacao = scanner.next().toUpperCase().charAt(0);
+            double valorTransacao = scanner.nextDouble();
+
+            if (tipoTransacao == 'D') {
+                saldo += valorTransacao;
+                transacoes.add(i + ". Deposito de " + valorTransacao);
+            } else if (tipoTransacao == 'S') {
+                saldo -= valorTransacao;
+                transacoes.add(i + ". Saque de " + valorTransacao);
+            } else {
+                System.out.println("Opção inválida. Utilize D para depósito ou S para saque.");
+                i--;
+            }
+        }
+
+        System.out.println("Saldo: " + saldo);
+        System.out.println("Transacoes:");
+        for (String t : transacoes){
+            System.out.println(t);
+        }
+
+        scanner.close();
+    }
+}
